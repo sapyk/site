a87df58839cc5ef57a91ca2350b49fff5398a61a
smsathyacharana <sm.sathyacharana@techsathya.in>
2026-03-28 13:53:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,30 +12,12 @@
   ctaLabel: "Send Enquiry",
   ctaHref: "contact.html#enquire",
   whatsappNumber: "919900727600",
-  whatsappMessage:
-    "Namasthe, I would like to know more about the pranayama classes at Sri Atmananda Pranayama Yoga Kendra.",
-  designedByName: "Tech SathyA",
-  designedByUrl: "https://techsathya.in",
-  emailJs: {
-    publicKey: "Re87m6KD4XksA-EhI",
-    serviceId: "service_xguonok",
-    templateId: "template_bbz7cnv",
-    autoReplyTemplateId: "template_hw5yama"
-  },
-  form: {
-    submitLabel: "Send Enquiry",
-    sendingLabel: "Sending...",
-    readyMessage: "Share your details and we will contact you soon.",
-    validationMessage: "Please complete all required fields before sending.",
-    successMessage: "Thank you. Your enquiry has been sent successfully.",
-    errorMessage: "There was a small delivery issue.",
-    fallbackMessage: "Your enquiry is being sent through our backup channel now."
-  },
+  whatsappMessage: "Hello, I would like to learn more about Sri Atmananda Pranayama Yoga Kendra.",
   nav: [
     { label: "Home", href: "index.html", page: "home" },
     { label: "About", href: "about.html", page: "about" },
     { label: "Programs", href: "programs.html", page: "programs" },
-	{ label: "Register", href: "register.html", page: "register" },
+    { label: "Register", href: "register.html", page: "register" },
     { label: "Gallery", href: "gallery.html", page: "gallery" },
     { label: "Contact", href: "contact.html", page: "contact" }
   ],
@@ -44,26 +26,13 @@
     hero: "assets/images/hero-sanctuary.png",
     guru: "assets/images/guru-portrait.png",
     story: "assets/images/story-space.png",
-    program: "assets/images/program-breath.png",
-    contact: "assets/images/contact-welcome.png",
-    upi: "assets/images/upi-code.png",
-    gallery: [
-      "assets/images/gallery-featured.jpg",
-      "assets/images/gallery-01.png",
-      "assets/images/gallery-02.png",
-      "assets/images/gallery-03.png",
-      "assets/images/gallery-04.png",
-	  "assets/images/gallery-05.png",
-      "assets/images/gallery-06.png"
-    ]
+    program: "assets/images/program-thumb.png"
   }
 };
 
-window.getConfigValue = function getConfigValue(path) {
-  return path.split(".").reduce(function readPart(value, key) {
-    if (value && Object.prototype.hasOwnProperty.call(value, key)) {
-      return value[key];
-    }
-    return undefined;
+/* Utility to read CONFIG properties by key path */
+window.getConfigValue = function (keyPath) {
+  return keyPath.split(".").reduce(function(acc, key) {
+    return acc && acc[key] !== undefined ? acc[key] : undefined;
   }, window.CONFIG);
 };
